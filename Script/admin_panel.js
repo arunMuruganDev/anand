99d26eee65ee8b269677f66626ba5d82219ce274
arunMuruganDev <121711565+arunMuruganDev@users.noreply.github.com>
2023-04-16 22:13:09 +0530

    Add files via upload

---
+++ Script/admin_panel.js
@@ -0,0 +1,32 @@
+const first = () =>{
+    document.querySelector(".first").style.display="block"
+    document.querySelector(".second").style.display="none"
+    document.querySelector(".third").style.display="none"
+    document.querySelector(".fourth").style.display="none"
+}
+
+// Second
+const second = () =>{
+    document.querySelector(".first").style.display="none"
+    document.querySelector(".second").style.display="block"
+    document.querySelector(".third").style.display="none"
+    document.querySelector(".fourth").style.display="none"
+}
+
+// Third
+const third = () =>{
+    document.querySelector(".first").style.display="none"
+    document.querySelector(".second").style.display="none"
+    document.querySelector(".third").style.display="block"
+    document.querySelector(".fourth").style.display="none"
+}
+
+// fourth
+const fourth = () =>{
+    document.querySelector(".first").style.display="none"
+    document.querySelector(".second").style.display="none"
+    document.querySelector(".third").style.display="none"
+    document.querySelector(".fourth").style.display="block"
+}
+
+// alert("hi")
